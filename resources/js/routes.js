fabecdf5342be15ff6c2c5e136711a1c7b0a1860
Jai <jai34782@gmail.com>
2021-03-01 14:29:04 +0800

    add file

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -5,7 +5,7 @@ Vue.use(VueRouter)
 import Administration from './components/Home'
 import Accounting from './components/Hello'
 import Purchasing from './components/Purchasing'
-
+ 
 
 export default new VueRouter({
     mode: 'history',
@@ -15,13 +15,11 @@ export default new VueRouter({
             name: 'Administration',
             component: Administration
         },
-
         {
             path: '/Accounting',
             name: 'Accounting',
             component: Accounting
         },
-
         {
             path: '/Purchasing', 
             name:'Purchasing', 
